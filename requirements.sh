185945c2b48bd7db42f57d49a298483f9da1082a
kharitz <32305176+kharitz@users.noreply.github.com>
2023-03-29 15:04:20 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,8 @@
 # This file may be used to create an environment for running this project:
 
-export LD_LIBRARY_PATH=/usr/pubsw/packages/11.3/lib64:$LD_LIBRARY_PATH
-export PATH=/usr/pubsw/packages/CUDA/11.3/bin:$PATH
-source /autofs/space/ballarat_001/users/kg149/anaconda3/etc/profile.d/conda.sh
-# export LD_LIBRARY_PATH=/PATH_TO_CUDA/11.3/lib64:$LD_LIBRARY_PATH
-# export PATH=/PATH_TO_CUDA/CUDA/11.3/bin:$PATH
+source PATH_TO_CONDA/conda.sh
+export LD_LIBRARY_PATH=/PATH_TO_CUDA/11.3/lib64:$LD_LIBRARY_PATH
+export PATH=/PATH_TO_CUDA/CUDA/11.3/bin:$PATH
 conda create -n proj_alignment python=3.9
 conda activate proj_alignment
 conda install -c pytorch pytorch=1.11.0 torchvision cudatoolkit=11.3
